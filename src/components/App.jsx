3bd5bd0dcb25de2dd71aa6445a2cf1e66091b6f0
Artem Maslii <a.masliy@ukr.net>
2023-01-23 18:18:07 +0200

    optimization code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,32 +5,11 @@ import Statistics from './Feedback/Statistics'
 import FeedbackOptions from './Feedback/FeedbackOptions'
 
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  const arrayButton = ['good', 'neutral', 'bad'];
-
-  const total = countTotalFeedback()
-  const positivePercentage = countPositiveFeedbackPercentage()
+  const [state, setState] = useState({ good: 0, neutral: 0, bad: 0 });
+  const {good, neutral, bad} = state
   
-  const countFeedback = ( data ) => {
-    switch(data) {
-      case 'good':
-        setGood(prevState => prevState + 1)
-        break;
-
-      case 'neutral':
-        setNeutral(prevState => prevState + 1)
-        break;
-
-      case 'bad':
-        setBad(prevState => prevState + 1)
-        break;
-
-      default:
-        return;
-    }
+  const countFeedback = (name) => {
+    setState(state => ({...state, [name]: state[name] + 1}))
   };
 
   function countTotalFeedback() {
@@ -45,16 +24,18 @@ const App = () => {
     }
   }
 
+  const options = Object.keys(state)
+
   return (
     <Section title="Please leave feedback">
-      <FeedbackOptions  options={ arrayButton }
+      <FeedbackOptions  options={ options }
                         onLeaveFeedback={countFeedback}>
       </FeedbackOptions>
       <Statistics good={good}
                   neutral={neutral}
                   bad={bad}
-                  total={total}
-                  positivePercentage={positivePercentage}>
+                  total={countTotalFeedback()}
+                  positivePercentage={countPositiveFeedbackPercentage()}>
       </Statistics> 
     </Section>
     );
